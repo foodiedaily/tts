3f510185879a0a098dbc37eb2c937238a9aa33d9
alipsmith <alipsmith@me.com>
2015-10-11 18:12:54 -0700

    added shop page, made header sticky and hidden. need to add sticky header to all pages

--- wp-content/themes/socialchef-child/js/custom-script.js
+++ wp-content/themes/socialchef-child/js/custom-script.js
@@ -1,26 +1,35 @@
 (function($) {
+
+	
+
 	$(document).ready(function(){
 
-	// $(window).scroll(function() {    
- //    var scroll = $(".head").scrollTop();    
- //    if (scroll < 10) {
- //        $(".stickyHeader").addClass("headerShow");
- //        // $(".stickyHeader").removeClass("headerHide");
- //    }else{
- //    	// $(".stickyHeader").addClass("headerHide");
- //    	$(".stickyHeader").removeClass("headShow");
- //    }
+		
+		function headerShow(){
+			if (!$("body").hasClass("home-page")) {
+				$(this).children("#headID").removeClass("headHide");
+				$(this).find("#headID").addClass("headShow");
+			}
+		};
+		headerShow();	
 
-// $(window).scroll(function() {
-// if ($(this).scrollTop() > 1){  
-//     $('.stickyHeader').addClass("sticky");
-//   }
-//   else{
-//     $('.stickyHeader').removeClass("sticky");
-//   }
-// });
-
-
-});
+		$(window).scroll(function() {		    
+	    var scroll = $(".home-page").scrollTop();    
+		    if (scroll > 0) {
+		       	$("#headID").addClass("headShow");
+		    	$("#headID").removeClass("headHide");
+		    }else if (scroll <= 0) {
+		    	$("#headID").addClass("headHide");
+		    	$("#headID").removeClass("headShow");
+			};
+		});
 	});
 })(jQuery);
+
+// if height of <body> is equal to height of window, show header
+
+// var bodyHeight = $("body").height();
+// if (bodyHeight) = 100
+
+
+// || (window.location.pathname != '/tts.dev')
